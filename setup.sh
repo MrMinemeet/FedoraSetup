e1f81e15372df7489604534a4dd548560a2bbccb
MrMinemeet <alex@wtf-my-code.works>
2024-02-27 14:46:34 +0100

    Check for errors and add end info
    Suggest user to restart system, and print info if an error occured

--- setup.sh
+++ setup.sh
@@ -72,8 +72,17 @@ alias="
 	alias compresspdf='gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=output.pdf'\n
 "
 
+exited_with_errors=false
+check_error() {
+    # Check the exit code
+    if [ $? -ne 0 ]; then
+        exited_with_errors=true
+    fi
+} 
+
 # Enable Flathub repository
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+check_error
 
 # Configure DNF
 echo "max_parallel_downloads=20" >> /etc/dnf/dnf.conf
@@ -81,33 +90,46 @@ echo "fastestmirror=True" >> /etc/dnf/dnf.conf
 
 # Install DNF mirrors
 dnf install -y $dnf_mirrors
+check_error
+
 # VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+check_error
 
 # Enable/Disable/Add repositories
 dnf config-manager --set-enabled rpmfusion-nonfree-steam
+check_error
 dnf config-manager --disable google-chrome
+check_error
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+check_error
 dnf copr disable phracek/PyCharm
+check_error
 
 # Remove unwanted packages
 dnf remove -y $dnf_remove_packages
+check_error
 
 # Upgrade currently installed packages
 dnf upgrade --refresh -y
+check_error
 
 # Install DNF packages
 dnf install -y $dnf_packages
+check_error
 
 # Install flatpak packages
 flatpak install -y $flatpak_packages
+check_error
 
 # Install official 7zip binary
 curl https://raw.githubusercontent.com/MrMinemeet/Install7zz/main/install.sh | sudo bash
+check_error
 
 # Install NVM
 sudo -u $SUDO_USER bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash'
+check_error
 
 # Install JetBrains Toolbox
 wget -O - "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA" | tar -xz -C /tmp/
@@ -116,9 +138,18 @@ rm -r /tmp/jetbrains*
 
 # Install Rust Toolchain via RustUp
 sudo -u $SUDO_USER bash -c 'wget -qO- https://sh.rustup.rs | sh -s -- -y'
+check_error
 
 # Add alias to .bashrc
 echo $alias >> "/home/$SUDO_USER/.bashrc"
 
-# Get updated .bashrc
-source "/home/$SUDO_USER/.bashrc"
+# Info for user
+echo ""
+echo "Installation finished."
+echo "Please reboot your system to apply all changes."
+
+
+if [ $exited_with_errors = true ]; then
+	# Set color to red, write info, then reset color
+	echo -e "\e[31mOError(s) occured during the installation process. Please check the output for more information.\e[0m"
+fi
